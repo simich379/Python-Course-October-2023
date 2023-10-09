ba3a7bc8f8e7137cdbd7ba32fb8c17e3571613bd
Simona Ilieva <simona.yako@gmail.com>
2023-10-09 21:53:06 +0300

    homeowrk 1 : add last changes

---
+++ homework1.py
@@ -0,0 +1,42 @@
+print("Hello world!");
+
+######################################
+# Problem 1:
+x = 10
+y = 2.5
+# addition of 2 variables
+sumOf = x + y
+print("sum is : %d" % sumOf)
+print("sum is : %2.2f" % sumOf)
+# multiplication of 2 variables
+productPf = x * y
+print("multiplication is : %d" % productPf)
+print("multiplication is : %2.2f" % productPf)
+# subtraction of 2 variables
+diff = x - y
+print("subtraction is : %d" % diff)
+print("subtraction is : %2.2f" % diff)
+# division of 2 variables
+div = x / y
+print("division is : %d" % div)
+print("division is : %2.2f" % div)
+######################################
+# Write a Python script that creates 5 string variables of 5 bulgarian PINs ( ЕГН ) and 5 names in
+# tuples. Finally, insert only the pairs whose name begins with a vowel in a dictionary and print it.
+egn = ("7305034321", "9910064536", "5103736893", "6302014789", "9010196754")
+names = ("Metodi", "Simona", "Elena", "Neli", "Adelina")
+result = ()
+this_dict = dict(zip(egn, names))
+print("Initial dictionary:", this_dict)
+# with list compression:
+# keys = [k for k, v in this_dict.items() if v[0] == 'A' or v[0] == 'E' or v[0] == 'O' or v[0] == 'I' or v[0] == 'U']
+# print("Filtered ", keys)
+newDict = dict()
+for (key, value) in this_dict.items():
+    if value[0] == 'A' or value[0] == 'E' or value[0] == 'O' or value[0] == 'I' or value[0] == 'U':
+        newDict[key] = value
+
+print("filtered dict:", newDict)
+
+###################################################
+
